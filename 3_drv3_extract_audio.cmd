f7e1ef36e3952c87f2080b782163a0ec3e24e978
Andrew Ashurow <0x384c0@gmail.com>
2017-11-20 19:05:23 +0600

    removed pauses

--- 3_drv3_extract_audio.cmd
+++ 3_drv3_extract_audio.cmd
@@ -1,7 +1,5 @@
 if not exist "partition_resident_win" mkdir "partition_resident_win"
 MOVE DR_assets\partition_resident_win\sound partition_resident_win
-pause
 python Danganronpa-Tools\drv3\acb_ex.py
-pause
 MOVE partition_resident_win\sound DR_assets\partition_resident_win
 pause
